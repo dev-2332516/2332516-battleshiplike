93094cb025c0e7a5928d8521afc3e1f74eb56281
Creeper <thecreepermdrl@gmail.com>
2025-09-26 12:27:33 -0400

    feat: retirer comms

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -12,38 +12,6 @@ namespace Client
             Themer.InitializeTheme();
             Console.Clear();
             ClientConnection.StartClient();
-            //int longueur, hauteur;
-            //bool valid = false;
-            //while (!valid)
-            //{
-            //    // Affiche les instructions et demande la taille
-            //    Console.WriteLine("La taille de la maquette doit être entre 4x4 et 12x12\n");
-            //    Console.Write("Longueur de la maquette: ");
-            //    longueur = Interface.AskForNumber();
-            //    Console.Write("\nHauteur de la maquette: ");
-            //    hauteur = Interface.AskForNumber();
-
-            //    // Verifier si la taille entrée est valide 
-            //    if (longueur >= 4 && longueur <= 12 && hauteur >= 4 && hauteur <= 12) valid = true;
-            //    else
-            //    {
-            //        // Affiche le message d'erreur
-            //        Console.Clear();
-            //        Console.ForegroundColor = ConsoleColor.Red;
-            //        Console.WriteLine("Taille entrée invalide");
-            //        Console.ForegroundColor = ConsoleColor.White;
-            //    }
-            //    Interface.Longueur = longueur;
-            //    Interface.Hauteur = hauteur;
-            //}
-            //while (true)
-            //{
-            //    Interface.Longueur = 6;
-            //    Interface.Hauteur = 5;
-            //    Console.Clear();
-            //    Interface.SelectGrid(2, 2);
-            //    Console.ReadLine();
-            //}
         }
     }
 
